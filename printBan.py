e99946470c3d40ab1a77896c6ccb13f1962d5831
fattredd <fattredd@gmail.com>
2013-08-01 21:02:41 -0400

    -Updated printBan.py to include an unpickler

--- printBan.py
+++ printBan.py
@@ -1,7 +1,16 @@
-import sys
+import sys, pickle
 
 def pBan(data):
+	data=data.replace('\r','')
 	for line in data:
 		for char, count in line:
 			sys.stdout.write(char * count)
 		sys.stdout.write("\n")
+
+def pLoad(name):
+	if not '.p' in name:
+		name += '.p'
+	f = open(name)
+	out = pickle.loads(f.read())
+	f.close()
+	return out
