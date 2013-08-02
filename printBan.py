77b1d31fdc56eba5f7aa8cc2c1b73bb51d72a2e9
fattredd <fattredd@gmail.com>
2013-08-02 10:14:13 -0400

    added cmd line support

--- printBan.py
+++ printBan.py
@@ -1,16 +1,16 @@
 import sys, pickle
 
 def pBan(data):
-	data=data.replace('\r','')
 	for line in data:
 		for char, count in line:
 			sys.stdout.write(char * count)
 		sys.stdout.write("\n")
 
 def pLoad(name):
-	if not '.p' in name:
-		name += '.p'
 	f = open(name)
 	out = pickle.loads(f.read())
 	f.close()
 	return out
+
+if __name__ == '__main__':
+	pBan(pLoad(sys.argv[1]))
